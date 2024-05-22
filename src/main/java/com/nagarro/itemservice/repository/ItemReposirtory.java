12a427459db84e01ccab09592be544953af9dbd4
Vipul Bhadouriya <vipul.bhadouriya@nagarro.com>
2024-05-22 20:18:11 +0530

    added parameters to repo

--- src/main/java/com/nagarro/itemservice/repository/ItemReposirtory.java
+++ src/main/java/com/nagarro/itemservice/repository/ItemReposirtory.java
@@ -11,9 +11,9 @@ import com.nagarro.itemservice.dto.Items;
 @Repository
 public class ItemReposirtory {
 
-	public List<Items> getAllItems() {
-		return Stream.of(new Items(151, "Samsung", "Mobile"), new Items(1062, "Home Appliace", "Washing Machine"),
-				new Items(13, "Home Appliace", "Air Conditioning"),new Items(3, "Apple", "Macbook pro laptop")).collect(Collectors.toList());
-	}
+    public List<Items> getAllItems() {
+        return Stream.of(new Items(151, "Samsung", "Mobile"), new Items(1062, "Home Appliace", "Washing Machine"),
+                new Items(13, "Home Appliace", "Air Conditioning"), new Items(8, "Hair", "Washing Machine"), new Items(3, "Apple", "Macbook pro laptop")).collect(Collectors.toList());
+    }
 
 }
