8aa3e51c3e44d284b40aea79ba6e4d742f898d68
Vipul Bhadouriya <vipul.bhadouriya@nagarro.com>
2024-05-23 14:36:10 +0530

    changes in repo

--- src/main/java/com/nagarro/itemservice/repository/ItemReposirtory.java
+++ src/main/java/com/nagarro/itemservice/repository/ItemReposirtory.java
@@ -12,7 +12,7 @@ import com.nagarro.itemservice.dto.Items;
 public class ItemReposirtory {
 
     public List<Items> getAllItems() {
-        return Stream.of(new Items(151, "Samsung", "Mobile"), new Items(1062, "Home Appliace", "Washing Machine"),
+        return Stream.of(new Items(151, "Samsung", "Mobile"),
                 new Items(13, "Home Appliace", "Air Conditioning"), new Items(8, "Hair", "Washing Machine"), new Items(3, "Apple", "Macbook pro laptop")).collect(Collectors.toList());
     }
 
